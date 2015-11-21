350a7d371d04c5bd90ba331ffbafbe710999a8ad
Tobias <m@dsen.tv>
2015-11-20 20:28:25 -0800

    Added initial Android app

---
+++ PartyOn/app/src/main/java/edu/csumb/partyon/activities/MainActivity.java
@@ -0,0 +1,91 @@
+package edu.csumb.partyon.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.csumb.partyon.R;
+import edu.csumb.partyon.fragments.DashboardFragment;
+
+/**
+ * Created by Tobias on 20.11.2015.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupDrawerLayout();
+        loadFragment();
+    }
+
+    private void setupDrawerLayout() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+
+                Fragment fragment;
+                String tag;
+
+                switch (menuItem.getItemId()){
+                    case R.id.drawer_home:
+                        fragment = getSupportFragmentManager().findFragmentByTag(DashboardFragment.TAG);
+                        if (fragment == null) {
+                            fragment = new DashboardFragment();
+                        }
+                        tag = DashboardFragment.TAG;
+                        break;
+                    default:
+                        fragment = getSupportFragmentManager().findFragmentByTag(DashboardFragment.TAG);
+                        if (fragment == null) {
+                            fragment = new DashboardFragment();
+                        }
+                        tag = DashboardFragment.TAG;
+                        break;
+                }
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment, tag)
+                        .commit();
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment(){
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new DashboardFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ PartyOn/app/src/main/java/edu/csumb/partyon/fragments/DashboardFragment.java
@@ -0,0 +1,41 @@
+package edu.csumb.partyon.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.csumb.partyon.R;
+
+/**
+ * Created by Tobias on 20.11.2015.
+ */
+public class DashboardFragment extends Fragment {
+
+    public static final String TAG = "MainFragment";
+
+    private Toolbar toolbar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        initToolbar();
+        return view;
+    }
+
+    private void initToolbar() {
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        final ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.app_title);
+        }
+    }
+}
